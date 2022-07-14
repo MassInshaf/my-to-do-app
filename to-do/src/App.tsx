3bee43a7dacb7e9925fa031223fff030888fda1a
MassInshaf <97073682+MassInshaf@users.noreply.github.com>
2022-07-14 11:03:49 +0530

    sample

--- to-do/src/App.tsx
+++ to-do/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-// commmmmmm
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React12345
+          Learn React
         </a>
       </header>
     </div>
